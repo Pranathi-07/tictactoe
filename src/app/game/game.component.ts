4e2db0968517e24805e21d8407b4ca6e486e73ed
Pranathi-07 <60241219+Pranathi-07@users.noreply.github.com>
2021-08-31 11:01:14 -0500

    Adding winner check conditions

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,33 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import {Status}  from '../gamestatus';
-import {Gameservice} from '../gameservice'
-
+import { Status } from '../gamestatus';
+import { Gameservice } from '../gameservice';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss'],
-  providers:[Gameservice]
+  providers: [Gameservice],
 })
 export class GameComponent implements OnInit {
   gridsize: any;
-  grids: any[] =[{id: 1, rows:3, columns:3}, {id: 2, rows:5, columns:5}];
-  radioSelected:any;
-  showBoard:boolean = false;
-  constructor(public game: Gameservice) { }
+  grids: any[] = [
+    { id: 1, rows: 3, columns: 3 },
+    { id: 2, rows: 5, columns: 5 },
+  ];
+  radioSelected: any;
+  showBoard: boolean = false;
+  constructor(public gameService: Gameservice) {}
   // gameStatus:boolean = false;
-  playerName1:string="";
-  playerName2:string="";
-  playerName3:string="";
-  playerName4:string="";
-  currentPlayerList5:string[]=[this.playerName1,this.playerName2,this.playerName3,this.playerName4];
-  currentPlayerList3:string[]=[this.playerName1,this.playerName2];
-  gameStatus:Status=Status.STOP;
+  playerName1: string = '';
+  playerName2: string = '';
+  playerName3: string = '';
+  playerName4: string = '';
+  currentPlayerList5: string[] = [
+    this.playerName1,
+    this.playerName2,
+    this.playerName3,
+    this.playerName4,
+  ];
+  currentPlayerList3: string[] = [this.playerName1, this.playerName2];
+  gameStatus: Status = Status.STOP;
 
-  ngOnInit(): void {
-  }
-  
-  startGame():void{
+  ngOnInit(): void {}
+
+  startGame(): void {
+    // this.gameService.setGameMode(this.radioSelected);
     this.showBoard = true;
     this.gameStatus = Status.START;
   }
@@ -36,13 +43,8 @@ export class GameComponent implements OnInit {
     this.showBoard = false;
   }
 
-  resetGame(){
+  resetGame() {
     this.showBoard = false;
     this.gameStatus = Status.STOP;
   }
 }
-  
-
-  
-
-
--- src/app/gameservice.ts
+++ src/app/gameservice.ts
@@ -1,45 +1,130 @@
-import { EventListenerFocusTrapInertStrategy } from '@angular/cdk/a11y';
-import {Status}  from './gamestatus';
+import { Status } from './gamestatus';
 
 export class Gameservice {
-    gameField: Array<number> = [];
-    currentPlayer:number = 1;
-    gameStatus: Status=Status.START;
-    count:number=0;
-    
+  gameMode: any;
+  gameField: string[][] = [];
+  Field: string[][] = [[], [], [], [], []];
+  currentPlayer: number = 1;
+  gameStatus: Status = Status.START;
+  count: number = 0;
 
-    setField(position:number, value:number):void{
-        this.gameField[position]=value;
-        console.log(this.gameField);
+  createArray(rows: any) {
+    for (let i = 0; i < rows.length; i++) {
+      let colValues = [];
+      for (let j = 0; j < rows.length; j++) {
+        colValues.push('');
+      }
+      this.gameField[i] = colValues;
     }
+  }
 
-    getPlayerColorClass(): string {
-        const colorClass= (this.currentPlayer % 2 ===0)?'teamtwo':'teamone';
-        return colorClass; 
+  setGameMode(gameMode: any) {
+    this.gameMode = gameMode.rows;
+
+    // this.gameField = Array(gameMode.rows).fill('');
+  }
+  setField(rowIndex: any, colIndex: any, value: number, rows: any): void {
+    if (rows.length === 3) {
+      let key = value === 1 ? 'O' : 'X';
+      this.gameField[rowIndex][colIndex] = key;
+    } else if (rows.length === 5) {
+      let key = value % 2 === 0 ? 'X' : 'O';
+      this.gameField[rowIndex][colIndex] = key;
     }
-    changePlayer(rows:any):void{
-        let length:any;
-        if(rows.length===5){length=4;}
-        else if(rows.length===3){length=2;}
-        this.count++;
-        if(this.currentPlayer<length)
-        this.currentPlayer+=1
-        else
-        this.currentPlayer=1 
-         
+  }
+
+  getPlayerColorClass(): string {
+    const colorClass = this.currentPlayer % 2 === 0 ? 'teamtwo' : 'teamone';
+    return colorClass;
+  }
+  changePlayer(rows: any): void {
+    let length: any;
+    if (rows.length === 5) {
+      length = 4;
+    } else if (rows.length === 3) {
+      length = 2;
     }
+    if (this.currentPlayer < length) this.currentPlayer += 1;
+    else this.currentPlayer = 1;
+    this.count++;
+  }
 
-    async checkGamefull(rows:any):Promise<boolean>{
+  async checkGamefull(rows: any): Promise<boolean> {
+    let isFull = true;
+    this.gameStatus = Status.STOP;
+    if (this.count < 8 && rows.length === 3) {
+      this.gameStatus = Status.START;
+      isFull = false;
+    } else if (this.count < 24 && rows.length === 5) {
+      this.gameStatus = Status.START;
+      isFull = false;
+    }
+    return isFull;
+  }
+  winnerCheck(rows: any, playerMark: any): boolean {
+    // horizontal check
+    let size = rows.length;
+    let winner: boolean = false;
+    for (let i = 0; i < size; i++) {
+      if (this.gameField[i][0] === playerMark) {
+        let j;
 
-        let isFull=true;
-        this.gameStatus=Status.STOP;
-        if(this.count<8 && rows.length===3){
-            this.gameStatus=Status.START;
-            isFull = false;
-        }else if(this.count<24 && rows.length===5){
-            this.gameStatus=Status.START;
-            isFull = false;  
+        for (j = 1; j < size; j++) {
+          if (this.gameField[i][j] !== playerMark) {
+            break;
+          }
         }
-        return isFull;
+        if (j === size) {
+          winner = true;
+          break;
+        }
+      }
     }
+    // vertical check
+    for (let i = 0; i < size; i++) {
+      if (this.gameField[0][i] === playerMark) {
+        let j;
+
+        for (j = 1; j < size; j++) {
+          if (this.gameField[j][i] !== playerMark) {
+            break;
+          }
+        }
+        if (j === size) {
+          winner = true;
+          break;
+        }
+      }
+    }
+    // diagonals check
+    if (!winner) {
+      let i;
+
+      for (i = 0; i < size; i++) {
+        if (this.gameField[i][i] !== playerMark) {
+          break;
+        }
+
+        if (i == size - 1) {
+          winner = true;
+          break;
+        }
+      }
+      if (!winner) {
+        for (i = 0; i < size; i++) {
+          if (this.gameField[i][size - 1 - i] !== playerMark) {
+            break;
+          }
+
+          if (i == size - 1) {
+            winner = true;
+            break;
+          }
+        }
+      }
+    }
+
+    this.gameStatus = winner ? Status.STOP : this.gameStatus;
+    return winner;
+  }
 }
